a5f0956feb46af319388c199e0c39ed2c70fc082
ahmedismail4494 <77131884+ahmedismail4494@users.noreply.github.com>
2022-04-19 19:18:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function toggle(){
+  let header = document.getElementById('header');
+  header.classList.toggle('active');
+}
+
+
+/* var link = document.querySelectorAll("link a");
+
+link.onclick = function () { "use strict";
+  link.classList.add("active");
+  link.nextSibling.classList.remove("active");
+} */
+
+
+
+
+
+
+
